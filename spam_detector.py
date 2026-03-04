5d764e3210e86b0c325e01e49e2e4bf90dd9d99d
Rae <aldrichpadilla404@gmail.com>
2026-03-05 02:43:30 +0800

    just saving

--- spam_detector.py
+++ spam_detector.py
@@ -3,6 +3,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, classification_report
+import joblib
 
 def load_data():
     url = "https://raw.githubusercontent.com/justmarkham/pycon-2016-tutorial/master/data/sms.tsv"
